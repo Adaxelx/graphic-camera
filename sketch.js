0d1ad7d0e94c6d8e7b8da65102caad4f668b4454
Adrian Mostowski <akmostowski@gmail.com>
2022-05-07 23:49:22 +0200

    update all events

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
 let mapOfPoints = {};
 let mapOf2dPoints = {};
 let connections = [];
-const translation = { x: 100, y: 100, z: 0 };
-let distance = 10;
-let rotateDeg = 0;
-let scale = 1;
+
+let distance = 1000;
+
 const {
   perspectiveProjection,
   rotateX3d,
@@ -18,10 +17,19 @@ const {
 const canvasWidth = 800;
 const canvasHeight = 800;
 
-function setup() {
-  const observerVector = createVector(canvasWidth / 2, canvasHeight);
+const rotationAxisParams = {
+  y: { A: 0, B: 1, C: 0 },
+  x: { A: 100, B: 1, C: 0 },
+  z: { A: 0, B: 1, C: 100 },
+};
 
-  console.log(observerVector);
+const params = {
+  rotation: { x: 0, y: 0, z: 0 },
+  translation: { x: 0, y: 0, z: 0 },
+  zoom: 1,
+};
+
+function setup() {
   createCanvas(canvasWidth, canvasHeight);
   fill(0);
 
@@ -38,16 +46,7 @@ function setup() {
           const [id, x, y, z] = line.split(" ").map((str) => Number(str));
           mapOfPoints[id] = { x, y, z };
           Object.entries(mapOfPoints).forEach(([key, value]) => {
-            // const rotated2 = rotateY3d(value, 30);
-
-            // const rotated1 = rotateZ3d(rotated2, 30);
-
-            // const rotated = rotateX3d(rotated1, 30);
-
-            mapOf2dPoints[key] = perspectiveProjection(
-              translatePoint(value, translation),
-              distance
-            );
+            mapOf2dPoints[key] = perspectiveProjection(value, distance);
           });
         } else if (titleCount === 2) {
           const [from, to] = line.split(" ");
@@ -59,105 +58,66 @@ function setup() {
     fr.readAsText(this.files[0]);
   });
 
-  document.getElementById("rotation").addEventListener("change", function (e) {
-    rotateDeg = e.target.value;
-    const rotationValue = e.target.value;
-    Object.entries(mapOfPoints).forEach(([key, value]) => {
-      const rotated2 = rotateY3d(
-        translatePoint(value, translation),
-        rotationValue
-      );
-
-      //   const rotated1 = rotateZ3d(rotated2, rotationValue);
-
-      //   const rotated = rotateX3d(rotated1, rotationValue);
-      console.log(rotated2);
-      mapOf2dPoints[key] = perspectiveProjection(rotated2, distance);
-    });
-    console.log(mapOf2dPoints);
-  });
-
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowRight") {
-      rotateDeg += 0.1;
+      params.rotation.y += 0.1;
     } else if (e.key === "ArrowLeft") {
-      rotateDeg -= 0.1;
-    } else if (e.key === "Enter") {
-      distance += 10;
-    } else if (e.key === "p") {
-      distance -= 10;
-    } else if (e.key === "q") {
-      scale += 0.1;
-    } else if (e.key == "w") {
-      scale -= 0.1;
+      params.rotation.y -= 0.1;
+    } else if (e.key === "ArrowUp") {
+      params.rotation.x += 0.1;
+    } else if (e.key === "ArrowDown") {
+      params.rotation.x -= 0.1;
+    } else if (e.key === "q" || e.key === "Q") {
+      params.rotation.z -= 0.1;
+    } else if (e.key === "e" || e.key === "E") {
+      params.rotation.z += 0.1;
+    } else if (e.key === "a" || e.key === "A") {
+      params.translation.x -= 10;
+    } else if (e.key === "d" || e.key === "D") {
+      params.translation.x += 10;
+    } else if (e.key === "w" || e.key === "W") {
+      params.translation.y -= 10;
+    } else if (e.key === "s" || e.key === "S") {
+      params.translation.y += 10;
+    } else if (e.key === "z" || e.key === "Z") {
+      params.translation.z -= 10;
+    } else if (e.key === "x" || e.key === "X") {
+      params.translation.z += 10;
+    } else if (e.key === "o" || e.key === "O") {
+      params.zoom += 0.1;
+    } else if (e.key === "p" || e.key === "P") {
+      params.zoom -= 0.1;
     }
-    // const rotationValue = e.target.value;
-    // Object.entries(mapOfPoints).forEach(([key, value]) => {
-    //   const rotated2 = rotateY3d(
-    //     translatePoint(value, translation),
-    //     rotationValue
-    //   );
-
-    //   //   const rotated1 = rotateZ3d(rotated2, rotationValue);
-
-    //   //   const rotated = rotateX3d(rotated1, rotationValue);
-    //   console.log(rotated2);
-    //   mapOf2dPoints[key] = perspectiveProjection(rotated2, distance);
-    // });
-    // console.log(mapOf2dPoints);
   });
 }
 
-let rotationValue = 0;
-let change = 0.1;
 function draw() {
   clear();
   translate(width / 2, height / 2);
   frameRate(5);
-  //   console.log(rotationValue);
   if (connections.length) {
     Object.entries(mapOfPoints).forEach(([key, value]) => {
-      //   console.log(
-      //     translatePoint(value, { x: 200 - value.x, y: 400 - value.y, z: 0 })
-      //   );
-      //   console.log(
-      //     rotateY3d(
-      //       translatePoint(value, { x: 200 - value.x, y: 400 - value.y, z: 0 }),
-      //       rotationValue
-      //     )
-      //   );
-      const rotated2 = translatePoint(
-        rotateY3d(
-          translatePoint(value, { x: 200 - value.x, y: 400 - value.y, z: 0 }),
-          rotationValue
-        ),
-        { x: value.x - 200, y: value.y - 400, z: 0 }
-      );
+      const T1 = { x: 0, y: 0, z: 0 };
+      let point = {
+        x: value.x + params.translation.x,
+        y: value.y + params.translation.y,
+        z: value.z + params.translation.z,
+      };
+      point = scalePoint(point, params.zoom);
 
-      const rotateObjY = { A: 0, B: 1, C: 0 };
-      const rotateObjX = { A: 1, B: 1, C: 0 };
-      const T1 = { x: width / 2, y: height, z: 0 };
-
-      const scaled = scalePoint(value, scale);
-
-      const rotatedPointY = rotatePointAroundLine(
-        scaled,
-        T1,
-        rotateObjY,
-        rotateDeg
-      );
-      const rotatedPointX = rotatePointAroundLine(
-        scaled,
-        T1,
-        rotateObjX,
-        rotateDeg
-      );
-
-      //   const rotated1 = rotateZ3d(rotated2, rotationValue);
-
-      //   const rotated = rotateX3d(rotated1, rotationValue);
-      mapOf2dPoints[key] = perspectiveProjection(rotatedPointX, distance);
+      Object.entries(params.rotation).forEach(([key, value]) => {
+        if (value) {
+          point = rotatePointAroundLine(
+            point,
+            T1,
+            rotationAxisParams[key],
+            params.rotation[key]
+          );
+        }
+      });
+      mapOf2dPoints[key] = perspectiveProjection(point, distance);
     });
+
     connections.forEach(({ from, to }) => {
       const { x: xFrom, y: yFrom } = mapOf2dPoints[from];
       const { x: xTo, y: yTo } = mapOf2dPoints[to];
@@ -165,12 +125,44 @@ function draw() {
       line(xFrom, yFrom, xTo, yTo);
       strokeWeight(1);
     });
-    rotationValue += change;
-
-    // noLoop();
   }
 }
 
-function mousePressed() {
-  clear();
-}
+// Not used
+
+const normalizePoints = () => {
+  let minX = Infinity;
+  let minY = Infinity;
+  let maxX = -Infinity;
+  let maxY = -Infinity;
+  Object.entries(mapOf2dPoints).forEach(([key, value]) => {
+    if (minX > value.x) {
+      minX = value.x;
+    }
+    if (minY > value.y) {
+      minY = value.y;
+    }
+    if (maxX < value.x) {
+      maxX = value.x;
+    }
+    if (maxY < value.y) {
+      maxY = value.y;
+    }
+  });
+  const diagonal = Math.sqrt(
+    Math.pow(maxX - minX, 2) + Math.pow(maxY - minY, 2)
+  );
+  const newMapOf2dPoints = Object.entries(mapOf2dPoints).reduce(
+    (prevObject, [key, value]) => {
+      return {
+        ...prevObject,
+        [key]: {
+          x: ((value.x - minX) / diagonal) * 400,
+          y: ((value.y - minY) / diagonal) * 400,
+        },
+      };
+    },
+    {}
+  );
+  mapOf2dPoints = newMapOf2dPoints;
+};
